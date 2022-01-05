363d3ccd4a81296df8a9eb6b5a34614329d2670c
TJKoury <TJKoury@gmail.com>
2022-01-05 15:31:43 -0500

    Create bitcoin-core-setup.sh

---
+++ bitcoin-core-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+VERSION=22.0
+BITCOINFOLDER=bitcoin-$VERSION
+BITCOINFILE=$BITCOINFOLDER-x86_64-linux-gnu.tar.gz
+
+useradd bitcoin
+mkdir -p /home/bitcoin
+chown -R bitcoin /home/bitcoin
+getent group bitcoin &> /dev/null || groupadd bitcoin
+
+if [ ! -f "$BITCOINFILE" ]; then
+    wget https://bitcoin.org/bin/bitcoin-core-$VERSION/$BITCOINFILE
+fi
+
+rm -rf $BITCOINFOLDER
+tar -xvf $BITCOINFILE
+
+cd $BITCOINFOLDER
+cp -R * /usr/local/
+
+curl https://raw.githubusercontent.com/bitcoin/bitcoin/master/contrib/init/bitcoind.service | sed 's/\/usr\//\/usr\/local\//' > /etc/systemd/system/bitcoind.service
+
+systemctl daemon-reload
+
+systemctl enable bitcoind
+
+systemctl stop bitcoind
+
+systemctl start bitcoind
+
+systemctl status bitcoind.service
